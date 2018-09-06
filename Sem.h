0958be1ef2497db23d3d7c7b0c5e46292877996e
Qu1et <807733144@qq.com>
2018-09-06 11:38:24 +0800

    Update Sem.h

--- Sem.h
+++ Sem.h
@@ -19,3 +19,38 @@ public:
 private:
 	sem_t sem;
 };
+
+#include <condition_variable>
+#include <mutex>
+	class Semaphore
+	{
+	public:
+		Semaphore(unsigned long count = 0) : m_count(count) {}
+ 
+		Semaphore(const Semaphore&) = delete;
+ 
+		Semaphore& operator=(const Semaphore&) = delete;
+ 
+		void Signal()
+		{
+			{
+				std::unique_lock<std::mutex> lock(m_mutex);
+				++m_count;
+			}
+			m_cv_uptr.notify_one();
+		}
+ 
+		void Wait()
+		{
+			std::unique_lock<std::mutex> lock(m_mutex);
+			while (m_count == 0) { // we may have spurious wakeup!
+				m_cv_uptr.wait(lock);
+			}
+			--m_count;
+		}
+ 
+	private:
+		std::mutex m_mutex;
+		std::condition_variable m_cv_uptr;
+		unsigned long m_count;
+	};
